26fd78d1d0dbd6269a6ebf7bfabb110c6eb64908
Jaspreet Singh <jaspreet.singh70077@gmail.com>
2023-12-26 18:04:29 +0530

    logger file added.

---
+++ src/config/loggerConfig.ts
@@ -0,0 +1,54 @@
+// loggerConfig.ts
+class FileTransportConfig {
+  filename: string;
+  level: string;
+
+  constructor(filename: string, level: string) {
+    this.filename = filename;
+    this.level = level;
+  }
+}
+
+class ConsoleTransportConfig {
+  level: string;
+
+  constructor(level: string) {
+    this.level = level;
+  }
+}
+
+class HttpTransportConfig {
+  level: string;
+
+  constructor(level: string) {
+    this.level = level;
+  }
+}
+
+class CustomTransportConfig {
+  handler: string;
+  level: string;
+
+  constructor(handler: string, level: string) {
+    this.handler = handler;
+    this.level = level;
+  }
+}
+
+class LoggerConfig {
+  level: string;
+  file?: FileTransportConfig;
+  console?: ConsoleTransportConfig;
+  http?: HttpTransportConfig;
+  custom?: CustomTransportConfig;
+
+  constructor(
+    level: string,
+    console?: ConsoleTransportConfig
+  ) {
+    this.level = level;
+    this.console = console;
+  }
+}
+
+export { LoggerConfig, FileTransportConfig, ConsoleTransportConfig, HttpTransportConfig, CustomTransportConfig };
