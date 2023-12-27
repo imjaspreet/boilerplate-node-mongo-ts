280191673532899079626915b20377a918b2228a
shambhu_bcoder <shambhunath@bcoder.in>
2023-12-27 19:32:49 +0530

    code fix

--- src/api/auths.ts
+++ src/api/auths.ts
@@ -10,43 +10,71 @@ import {
   changePassword,
 } from '../services/auths'
 export const signup = async (req: Request, res: Response): Promise<void> => {
-  const user = await register(req.body)
+  try {
+    const user = await register(req.body)
 
-  res.status(httpStatus.CREATED).send({ isSuccess: true, data: user })
+    res.status(httpStatus.CREATED).send({ isSuccess: true, data: user })
+  } catch (error) {
+    res.send({ isSuccess: false, error })
+  }
 }
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-  const user = await accountLogin(req.body)
-  res.status(httpStatus.OK).send({ isSuccess: true, data: user })
+  try {
+    const user = await accountLogin(req.body)
+    res.status(httpStatus.OK).send({ isSuccess: true, data: user })
+  } catch (error) {
+    res.send({ isSuccess: false, error })
+  }
 }
 
 export const verify = async (req: Request, res: Response): Promise<void> => {
-  const user = await verification(req.body.userId, req.body.code)
-  res.status(httpStatus.OK).send({ isSuccess: true, data: user })
+  try {
+    const user = await verification(req.body.userId, req.body.code)
+    res.status(httpStatus.OK).send({ isSuccess: true, data: user })
+  } catch (error) {
+    res.send({ isSuccess: false, error })
+  }
 }
 
 export const forgot = async (req: Request, res: Response): Promise<void> => {
-  const user = await forgotPassword(req.body)
-  res.status(httpStatus.OK).send({ isSuccess: true, data: user })
+  try {
+    const user = await forgotPassword(req.body)
+    res.status(httpStatus.OK).send({ isSuccess: true, data: user })
+  } catch (error) {
+    res.send({ isSuccess: false, error })
+  }
 }
 
 export const resendOtp = async (req: Request, res: Response): Promise<void> => {
-  const entity = await resendCode(req.params.id)
-  res.status(httpStatus.OK).send({ isSuccess: true, message: entity })
+  try {
+    const entity = await resendCode(req.params.id)
+    res.status(httpStatus.OK).send({ isSuccess: true, message: entity })
+  } catch (error) {
+    res.send({ isSuccess: false, error })
+  }
 }
 export const reset = async (req: Request, res: Response): Promise<void> => {
-  const user = await resetPassword(req.params.id, req.body.password)
-  res.status(httpStatus.OK).send({ isSuccess: true, message: user })
+  try {
+    const user = await resetPassword(req.params.id, req.body.password)
+    res.status(httpStatus.OK).send({ isSuccess: true, message: user })
+  } catch (error) {
+    res.send({ isSuccess: false, error })
+  }
 }
 
 export const updatePassword = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
-  const user = await changePassword(
-    req.params.id,
-    req.body.password,
-    req.body.newPassword,
-  )
-  res.status(httpStatus.OK).send({ isSuccess: true, message: user })
+  try {
+    const user = await changePassword(
+      req.params.id,
+      req.body.password,
+      req.body.newPassword,
+    )
+    res.status(httpStatus.OK).send({ isSuccess: true, message: user })
+  } catch (error) {
+    res.send({ isSuccess: false, error })
+  }
 }
