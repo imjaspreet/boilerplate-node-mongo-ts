da397445b27286ed114ef8ffd0de9f107e5a85f5
shambhu_bcoder <shambhunath@bcoder.in>
2023-12-27 18:15:08 +0530

    code fix

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
+import * as http from 'http'
 import { setGlobalEnvironment } from './global'
 import app from './app'
 import Environment from './environments/environment'
 import createLogger from './helpers/logger'
 import mongoose from 'mongoose'
-
 const logger = createLogger('Index.ts')
 
 const env: Environment = new Environment()
 setGlobalEnvironment(env)
 
-let server: any
+let server: http.Server | undefined
 
 mongoose.connect(env.mongodb.url).then(() => {
   server = app.listen(env.port, () => {
     logger.info(`Listening to port ${env.port}`)
+    console.log(`Listening to port ${env.port}`)
   })
+  console.log('mongodb Connected successfully')
 })
 
 const exitHandler = () => {
   if (server) {
     server.close(() => {
-      // logger.info('Server closed');
+      logger.info('Server closed')
       process.exit(1)
     })
   } else {
@@ -28,8 +30,8 @@ const exitHandler = () => {
   }
 }
 
-const unexpectedErrorHandler = (error: string) => {
-  // logger.error(error);
+const unexpectedErrorHandler = (error: Error) => {
+  logger.error(error)
   exitHandler()
 }
 
