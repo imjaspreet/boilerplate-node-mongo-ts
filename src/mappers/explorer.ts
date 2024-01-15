318002b15050da2fa26b97b0c5d208926ab81236
shambhu_bcoder <shambhunath@bcoder.in>
2024-01-15 15:31:42 +0530

    mapper updated

--- src/mappers/explorer.ts
+++ src/mappers/explorer.ts
@@ -5,6 +5,9 @@ export const toModel = (entity: IExplorerDoc) => {
     id: entity._id,
     name: entity.name,
     city: entity.city,
+    title: entity.title,
+    description: entity.description,
+    shortDescription: entity.shortDescription,
     country: entity.country,
     importance: entity.importance,
     latitude: entity.latitude,
@@ -23,24 +26,27 @@ export const toSearchModel = entities => {
   return _.map(entities, toModel)
 }
 
-export const toNewModel = (entity: IExplorerDoc) => {
+export const toNewModel = (body: IExplorerDoc) => {
   const model = {
-    id: entity._id,
-    name: entity.name,
-    city: entity.city,
-    country: entity.country,
-    importance: entity.importance,
-    latitude: entity.latitude,
-    longitude: entity.longitude,
-    additionalSources: entity.additional_sources,
-    categories: entity.categories && _.uniq(entity.categories),
+    id: body._id,
+    name: body.name,
+    city: body.city,
+    country: body.country,
+    importance: body.importance,
+    latitude: body.latitude,
+    longitude: body.longitude,
+    title: body.title,
+    description: body.description,
+    shortDescription: body.shortDescription,
+    additionalSources: body.additional_sources,
+    categories: body.categories && _.uniq(body.categories),
     location: {
       type: 'Point',
-      coordinates: [entity.longitude, entity.latitude],
+      coordinates: [body.longitude, body.latitude],
     },
-    properties: entity.properties,
-    distance: entity.distance,
-    foreignSources: entity.foreign_sources,
+    properties: body.properties,
+    distance: body.distance,
+    foreignSources: body.foreign_sources,
   }
   return model
 }
